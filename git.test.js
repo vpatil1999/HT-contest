b0436cb15a75fac39aedf807e46ac6f0048d6fd6
vaibhav <vpatil@woyce.io>
2025-06-03 11:20:38 +0530

    uat-4 take created for cherry pick in staging

--- git.test.js
+++ git.test.js
@@ -1,3 +1,4 @@
 console.log('Create new branch ---> , Name of branch : from-uat-1')
 console.log('Create new branch ---> , Name of branch : uat-2')
 console.log('Create new branch ---> , Name of branch : uat-3')
+console.log('Create new branch ---> , Name of branch : uat-4 take cherry pick to staging')

74567606c9712d96ccf3e998b74e8a7cc0de144c
vaibhav <vpatil@woyce.io>
2025-06-03 11:18:17 +0530

    branch-uat3

--- git.test.js
+++ git.test.js
@@ -1,2 +1,3 @@
 console.log('Create new branch ---> , Name of branch : from-uat-1')
 console.log('Create new branch ---> , Name of branch : uat-2')
+console.log('Create new branch ---> , Name of branch : uat-3')
